f2b6fadd3dbadb65d5d6610f2a769d56f26450d5
Aghiad Alzein <aghiad.alzein@gmail.com>
2020-10-31 01:29:48 +0200

    still working

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const print = require("./utility.js");
 const app = express();
+const path = require("path");
+
+//will automatically route to any file inside the public folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  print("Server running at port:", PORT);
+  print(`Server started at ${PORT}`);
 });
